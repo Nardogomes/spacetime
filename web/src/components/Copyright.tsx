911a9e5178d4d5b013afefdd646475fe11f2dd84
Ednardo Gomes <ednardo@email.com>
2023-05-19 11:31:35 -0300

    Criando a autenticação pelo Github e refatorando componentes.

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito na NLW🚀 por{' '}
+      <a
+        href="http://github.com/Nardogomes"
+        target="_blank"
+        rel="noreferrer"
+        className="underline transition-colors hover:text-gray-100"
+      >
+        Ednardo Gomes
+      </a>
+    </div>
+  )
+}
